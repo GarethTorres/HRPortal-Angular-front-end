a3365ba7d1a24cfd3cb77b093f805574ae5aabf3
bobwen0205 <wenmingzhi10@gmail.com>
2024-08-26 18:46:26 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    // title = 'HR Portal: HR Management';
-    constructor(private router: Router) {
-        router.events.subscribe(event => {
-          console.log("Navigation Event:", event);
-        });
-      }
+export class AppComponent implements OnInit {
+  // title = 'HR Portal: HR Management';
+  isAuthenticated: boolean = false;
+  constructor(private router: Router, private authService: AuthService) {
+    router.events.subscribe(event => {
+      console.log("Navigation Event:", event);
+    });
+  }
+
+  ngOnInit() {
+    this.authService.user.pipe(
+      map(user => !!user)
+    ).subscribe(authenticated => {
+      this.isAuthenticated = authenticated;
+    });
+  }
 }
